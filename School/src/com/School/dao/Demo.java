d0997f1163614ff8a1a46f7dc6ae92d6c8d68197
sreeja <sreejaanumulapuri301@gmail.com>
2026-05-19 11:58:55 +0000

    Add file

---
+++ School/src/com/School/dao/Demo.java
@@ -0,0 +1,8 @@
+package com.School.dao;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
--- School/src/com/School/dao/Teacher.java
+++
@@ -1,9 +0,0 @@
-package com.School.dao;
-
-public class Teacher {
-	public static void addTeacher(String name, String subject) {
-		// Code to add a teacher to the database
-		System.out.println("Teacher " + name + " added with subject " + subject);
-	}
-
-}
